ee6852fa594be7e280d11eb4162f111287ee6b99
Lilah Gresham <lpgresham@icloud.com>
2024-11-12 08:39:16 -0600

    started base code, need to debug and pull functions

--- src/project.py
+++ src/project.py
@@ -1,19 +1,33 @@
 import maya.cmds as cmds
 import random
 
-#subdivisions
-#plane size
-#maxheight
-#define land
-#Vertex Boolean
-#Count Vertexes
+landsub = 100
+landsize = 400
+maxheight = 15
+land = cmds.polyPlane(sx = landsub, sy = landsub, 
+                      w = landsize, h = landsize)
+vertex_nb = cmds.polyEvaluate(v=True)
+vertex_Count = range(vertex_nb)
 
-#Create list of values with in range, count valueus
-#create seed
+values = []
+high_values = [random.triangular(0,1,0)
+                for i in range(vertex_nb/10)]
+low_values = [random.uniform(0,0.3)
+              for i in range(vertex_nb/2)]
+values = low_values + high_values
+values_count = len(values)
+SEED = 448
+random.seed(SEED)
+random.shuffle(vertex_Count)
+random.shuffle(values)
+optimize_setter = []
 
-#loop through each value and transform vertex
-#Set Optimmize attribute
-#Select plane
+for x in vertex_Count:
+    mod = x % values_count
+    # cmds.move(values[mod-1]*maxheight, land[0] + '.vtx[' + str(x) + ']', y=True, absolute=True)
+    optimize_setter += [float(0), values[mod-1]*maxheight,float(0)]
+cmds.setAttr('pPlane1.vtx[:]', *optimize_setter)
+cmds.select(land[0])
 
 if __name__ == "__main__":
     main()
