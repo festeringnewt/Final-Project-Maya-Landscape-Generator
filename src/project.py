5bcf4ef9b8c2d1542160c20bf1858db5c616f28a
Lilah Gresham <lpgresham@icloud.com>
2024-11-12 08:22:29 -0600

    started pseudocode

--- src/project.py
+++ src/project.py
@@ -1,36 +1,19 @@
 import maya.cmds as cmds
 import random
 
-def generate_mountain(width, height, num_peaks, smoothness):
+#subdivisions
+#plane size
+#maxheight
+#define land
+#Vertex Boolean
+#Count Vertexes
 
-    plane = cmds.polyPlane(width=width, height= height, 
-                           subdivisionsX=width, subdivisionsY=height)[0]
-    peak_heights = [random.uniform(0,height) for n in range(num_peaks)]
-    for x in range(width+1):
-        for y in range(height+1):
-            print(f"Processing vertex ({x},{y})")
-            vertex_pos = cmds.pointPosition(f"{plane}.vtx[{x}][{y}]")
-            for i in range(num_peaks):
-                peak_x = random.uniform(0, width)
-                peak_y = random.uniform(0,height)
-                distance = ((x - peak_x)**2 + (y- peak_y)**2)**0.5
-                influence = max(0, smoothness - distance)
-                vertex_pos[2] += peak_heights[i] * influence/smoothness
-            
-            print(f'Before move: {vertex_pos}')
-            cmds.move(vertex_pos[0], vertex_pos[1], vertex_pos[2],
-                      f"{plane}.vtx[{x}][{y}]",)
-            print(f"After move: {cmds.pointPosition(f'{plane}.vtx[{x}][{y}]')}")
-            
-    return plane
+#Create list of values with in range, count valueus
+#create seed
 
-def main():
-    width = int(input("Width: "))
-    height = int(input("Height: "))
-    num_peaks = int(input("Number of peaks: "))
-    smoothness = int(input("smoothness: "))
-
-    generate_mountain(width, height, num_peaks, smoothness)
+#loop through each value and transform vertex
+#Set Optimmize attribute
+#Select plane
 
 if __name__ == "__main__":
     main()
