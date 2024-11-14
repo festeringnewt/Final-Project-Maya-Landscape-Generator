b6c7504cb9b42368db410aa4828d6f07c8708096
Lilah Gresham <lpgresham@icloud.com>
2024-11-14 15:07:53 -0600

    pulled functions from main code, organized.

--- src/project.py
+++ src/project.py
@@ -1,33 +1,37 @@
 import maya.cmds as cmds
 import random
 
-landsub = 100
-landsize = 400
-maxheight = 15
-land = cmds.polyPlane(sx = landsub, sy = landsub, 
+def generate_terrain(landsub, landsize, maxheight):
+    land = cmds.polyPlane(sx = landsub, sy = landsub, 
                       w = landsize, h = landsize)
-vertex_nb = cmds.polyEvaluate(v=True)
-vertex_Count = list(range(vertex_nb))
+    vertex_nb = cmds.polyEvaluate(v=True)
+    vertex_Count = list(range(vertex_nb))
+    
+    values = []
+    high_values = [random.triangular(0,1,0)
+                    for i in range(int(vertex_nb/10))]
+    low_values = [random.uniform(0,0.3)
+                for i in range(int(vertex_nb/2))]
+    values = low_values + high_values
+    values_count = len(values)
+    SEED = 448
+    random.seed(SEED)
+    random.shuffle(vertex_Count)
+    random.shuffle(values)
+    optimize_setter = []
 
-values = []
-high_values = [random.triangular(0,1,0)
-                for i in range(int(vertex_nb/10))]
-low_values = [random.uniform(0,0.3)
-              for i in range(int(vertex_nb/2))]
-values = low_values + high_values
-values_count = len(values)
-SEED = 448
-random.seed(SEED)
-random.shuffle(vertex_Count)
-random.shuffle(values)
-optimize_setter = []
+    for x in vertex_Count:
+        mod = x % values_count
+        # cmds.move(values[mod-1]*maxheight, land[0] + '.vtx[' + str(x) + ']', y=True, absolute=True)
+        optimize_setter += [float(0), values[mod-1]*maxheight,float(0)]
+    cmds.setAttr('pPlane1.vtx[:]', *optimize_setter)
+    cmds.select(land[0])
 
-for x in vertex_Count:
-    mod = x % values_count
-    # cmds.move(values[mod-1]*maxheight, land[0] + '.vtx[' + str(x) + ']', y=True, absolute=True)
-    optimize_setter += [float(0), values[mod-1]*maxheight,float(0)]
-cmds.setAttr('pPlane1.vtx[:]', *optimize_setter)
-cmds.select(land[0])
+def main():
+    landsize = int(input("Size of land: "))
+    landsub = int(input("Subdivisions: "))
+    maxheight = int(input("Max Height: "))
+    generate_terrain(landsub, landsize, maxheight)
 
 if __name__ == "__main__":
     main()
